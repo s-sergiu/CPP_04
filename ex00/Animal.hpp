7a7b8620792aab208bda935b4f433215af9ed86f
Sergiu Ster <ssergiu@student.42heilbronn.de>
2023-07-21 05:16:59 +0200

    udpate

--- ex00/Animal.hpp
+++ ex00/Animal.hpp
@@ -9,12 +9,12 @@ class Animal
 	protected:
 		std::string type;
 	public:
-		virtual void makeSound() const;
 		std::string getType() const;
 		Animal();
 		Animal(const Animal &copy);
 		Animal& operator = (const Animal &src);
 		virtual ~Animal();
+		virtual void makeSound() const;
 };
 
 #endif
